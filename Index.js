589be07875862a2eb784bc51a9be5bb53f603951
Vasantharaja <vasantharaja200295@gmail.com>
2024-02-10 12:06:30 +0530

    Completed code

--- Index.js
+++ Index.js
@@ -1,56 +1,47 @@
-const qrcode = require("qrcode-terminal");
-const { Client, RemoteAuth } = require("whatsapp-web.js");
-const { MongoClient } = require("mongodb");
+const { Client, RemoteAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-require("dotenv").config();
-
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
 
 
 
-mongoClient.connect()
-    .then(() => {
-        console.log('Connected to MongoDB');
-        const sessionsStore = mongoClient.db('Secure');
-        const client = new Client({
-            session: {
-                // Implement your own session storage using MongoDB
-                store: async (key, value) => {
-                    await sessionsStore.updateOne({ _id: key }, { $set: { value } }, { upsert: true });
-                },
-                retrieve: async (key) => {
-                    const session = await sessionsStore.findOne({ _id: key });
-                    return session ? session.value : null;
-                },
-            },
-        });
+mongoose.connect("mongodb://localhost:27017").then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000,
+        })
+    });
 
-        Authenticate(client);
-        const {sessionStatus, readyStatus} = CheckStatus(client);
-        console.log('Session Status:', sessionStatus);
-        console.log('Ready Status:', readyStatus);
 
-        client.initialize();
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+    });
 
+    client.on('remote_session_saved', ()=>{
+        console.log('Session saved!');
     })
-    .catch(error => {
-        console.error('Error connecting to MongoDB:', error);
+    
+    client.on('ready', () => {
+        console.log('Client is ready!');
     });
-
-
-function Authenticate(client) {
-    client.on('qr', qr => {
-        qrcode.generate(qr, {small: true});
+    
+    let tirgger = ['Hey', "hello", 'Hi', 'hi', 'Hello', 'Oi', 'oi', 'Hoi', 'hoi', 'hey', 'hoii', 'Hoii', 'oii', 'Oii'] 
+    let contacts = ['919629579216', '917010846735']
+    let count = 0;
+    client.on('message', async (message) => {
+        if(contacts.includes(message.from.split('@')[0])){
+            if (tirgger.includes(message.body)) {
+                await message.reply('What!');
+                count++;
+            }else if(count>0){
+                await client.sendMessage(message.from, 'Bye!');
+                count = 0;
+            } 
+        }
     });
-}
-
-function CheckStatus(client) {
-    let sessionStatus, readyStatus;
-    client.on('remote_session_saved',()=>{
-        sessionStatus= true;
-    });
-    client.on('ready',()=>{
-        readyStatus = true;
-    });
-    return {sessionStatus, readyStatus};
-}
+    
+    client.initialize();
+});
